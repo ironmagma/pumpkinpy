baf310cdf7911f2222d71259ff1a12745139e6b0
Philip Peterson <carniojack424@gmail.com>
2011-05-21 15:58:22 -0400

    Added rpartition for PHP

--- pumpkinpy.php
+++ pumpkinpy.php
@@ -4,7 +4,7 @@ function partition($haystack, $needle) {
    $nedlen = strlen($needle);
    $nedpos = strpos($haystack, $needle);
 
-   if ($nedpos == -1) {
+   if ($nedpos !== FALSE) {
       $before = substr($haystack, 0, $nedpos);
       $after = substr($haystack, $nedpos+$nedlen);
    } else {
@@ -13,6 +13,22 @@ function partition($haystack, $needle) {
       $after = "";
    }
 
+   return array($before, $needle, $after);
+}
+
+function rpartition($haystack, $needle) {
+   $nedlen = strlen($needle);
+   $nedpos = strrpos($haystack, $needle);
+    
+   if ($nedpos !== FALSE) {
+      $before = substr($haystack, 0, $nedpos);
+      $after = substr($haystack, $nedpos+$nedlen);
+   } else {
+      $before = "";
+      $needle = "";
+      $after = $haystack;
+   }
+    
    return array($before, $needle, $after);
 }
 
