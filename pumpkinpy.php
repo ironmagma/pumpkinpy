6073d2ee8473b6847bb20e87ab6aa434ca9ffcdd
Philip Peterson <carniojack424@gmail.com>
2011-05-24 19:42:53 -0400

    Added PHP endswith()

--- pumpkinpy.php
+++ pumpkinpy.php
@@ -43,3 +43,16 @@ function endswith($haystack, $needle) {
    }
    return FALSE;
 }
+
+
+function startswith($haystack, $needle) {
+   $needlen = strlen($needle);
+   if ($needlen === 0) {
+      return TRUE;
+   }
+   $pos = strrpos($haystack, $needle);
+   if ($pos === 0) {
+      return TRUE;
+   }
+   return FALSE;
+}
