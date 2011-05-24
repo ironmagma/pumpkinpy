a7a3087bb1498032a5371f109dc0cb7154778a06
Philip Peterson <carniojack424@gmail.com>
2011-05-24 14:15:16 -0400

    Added PHP endswith().

--- pumpkinpy.php
+++ pumpkinpy.php
@@ -32,3 +32,14 @@ function rpartition($haystack, $needle) {
    return array($before, $needle, $after);
 }
 
+function endswith($haystack, $needle) {
+   $needlen = strlen($needle);
+   if ($needlen === 0) {
+      return TRUE;
+   }
+   $pos = strrpos($haystack, $needle);
+   if ($pos === strlen($haystack) - $needlen) {
+      return TRUE;
+   }
+   return FALSE;
+}
