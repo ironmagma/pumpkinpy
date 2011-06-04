30141effebdac94de82ddabf4ebcda49d8c05b59
Philip Peterson <carniojack424@gmail.com>
2011-06-04 02:50:39 -0400

    Moved gitignore to base and finished up legal stuff (I think).

--- js/src/partition.js
+++ js/src/partition.js
@@ -1,3 +1,29 @@
+/*
+
+Legal boing crap follows. In simple english, you can use this
+code in your own project, be your project commercial or free.
+Just be sure to include the license and stuff. The "copyright"
+here is just for technical reasons.
+
+Copyright 2011, Philip Peterson.
+
+This file is part of Pumpkinpy.
+
+Pumpkinpy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Pumpkinpy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Pumpkinpy.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 String.prototype.partition = function(needle) {
    var haystack = this;
    var nedpos = haystack.indexOf(needle);
@@ -6,7 +32,7 @@ String.prototype.partition = function(needle) {
 
    if (nedpos !== -1) {
       before = haystack.substring(0, nedpos);
-      after = haystack.substring(nedpos+nedlen)
+      after = haystack.substring(nedpos+nedlen);
    } else {
       before = haystack;
       needle = "";
